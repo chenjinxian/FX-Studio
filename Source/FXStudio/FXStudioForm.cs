876c0fa27128880aec120b379988cc46efc912d3
chenjinxian <alanchen1203@gmail.com>
2016-09-08 21:34:51 +0800

    todo

--- Source/FXStudio/FXStudioForm.cs
+++ Source/FXStudio/FXStudioForm.cs
@@ -22,9 +22,9 @@ namespace FXStudio
             try
             {
                 IntPtr hInstance = Marshal.GetHINSTANCE(this.GetType().Module);
-//                 IntPtr hwnd = this.displayPanel.Handle;
-//                 RenderMethods.CreateInstance(hInstance, IntPtr.Zero, hwnd, 1, this.displayPanel.Width, this.displayPanel.Height);
-//                 m_messageHandler = new MessageHandler(this.displayPanel, this);
+                IntPtr hwnd = this.dockPanel.Handle;
+                RenderMethods.CreateInstance(hInstance, IntPtr.Zero, hwnd, 1, this.dockPanel.Width, this.dockPanel.Height);
+                m_messageHandler = new MessageHandler(this.dockPanel, this);
             }
             catch (Exception e)
             {

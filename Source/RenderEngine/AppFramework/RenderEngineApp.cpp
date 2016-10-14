eefbc526c3798a5bf9a40c5c1f0301cfb2d0f9f9
chenjinxian <alanchen1203@gmail.com>
2016-10-14 10:01:56 +0800

    to do

--- Source/RenderEngine/AppFramework/RenderEngineApp.cpp
+++ Source/RenderEngine/AppFramework/RenderEngineApp.cpp
@@ -9,55 +9,6 @@
 #include "../UserInterface/HumanView.h"
 #include "../UserInterface/MessageBox.h"
 
-CDXUTDialog                         g_HUD;                  // manages the 3D UI
-CDXUTDialog                         g_SampleUI;             // dialog for sample specific controls
-
-float                               g_fModelWaviness = 0.0f;
-bool                                g_bSpinning = true;
-
-#define IDC_TOGGLEFULLSCREEN    1
-#define IDC_TOGGLEREF           2
-#define IDC_CHANGEDEVICE        3
-#define IDC_TOGGLEWARP          4
-#define IDC_TOGGLESPIN          5
-#define IDC_PUFF_SCALE          6
-#define IDC_PUFF_STATIC         7
-
-void CALLBACK OnGUIEvent(UINT nEvent, int nControlID, CDXUTControl* pControl, void* pUserContext)
-{
-	switch (nControlID)
-	{
-	case IDC_TOGGLEFULLSCREEN:
-		DXUTToggleFullScreen();
-		break;
-	case IDC_TOGGLEREF:
-		DXUTToggleREF();
-		break;
-	case IDC_CHANGEDEVICE:
-		break;
-	case IDC_TOGGLEWARP:
-		DXUTToggleWARP();
-		break;
-
-	case IDC_TOGGLESPIN:
-	{
-		g_bSpinning = g_SampleUI.GetCheckBox(IDC_TOGGLESPIN)->GetChecked();
-		break;
-	}
-
-	case IDC_PUFF_SCALE:
-	{
-		g_fModelWaviness = (float)(g_SampleUI.GetSlider(IDC_PUFF_SCALE)->GetValue() * 0.01f);
-
-		WCHAR sz[100];
-		swprintf_s(sz, 100, L"Waviness: %0.2f", g_fModelWaviness);
-		g_SampleUI.GetStatic(IDC_PUFF_STATIC)->SetText(sz);
-
-		break;
-	}
-	}
-}
-
 #define MAX_LOADSTRING 100
 
 RenderEngineApp* g_pApp = nullptr;
@@ -162,28 +113,9 @@ bool RenderEngineApp::InitInstance(HINSTANCE hInstance, LPWSTR lpCmdLine, HWND h
 
 	DXUTInit(true, true, lpCmdLine, true);
 
-	g_fModelWaviness = 0.0f;
-	g_bSpinning = true;
-
-	g_HUD.Init(&D3DRenderer::m_DialogResourceManager);
-	g_SampleUI.Init(&D3DRenderer::m_DialogResourceManager);
-
-	g_HUD.SetCallback(OnGUIEvent); int iY = 10;
-	g_HUD.AddButton(IDC_TOGGLEFULLSCREEN, L"Toggle full screen", 0, iY, 170, 22);
-	g_HUD.AddButton(IDC_CHANGEDEVICE, L"Change device (F2)", 0, iY += 26, 170, 22, VK_F2);
-	g_HUD.AddButton(IDC_TOGGLEREF, L"Toggle REF (F3)", 0, iY += 26, 170, 22, VK_F3);
-	g_HUD.AddButton(IDC_TOGGLEWARP, L"Toggle WARP (F4)", 0, iY += 26, 170, 22, VK_F4);
-
-	g_SampleUI.SetCallback(OnGUIEvent); iY = 10;
-
-	WCHAR sz[100];
-	iY += 24;
-	swprintf_s(sz, 100, L"Waviness: %0.2f", g_fModelWaviness);
-	g_SampleUI.AddStatic(IDC_PUFF_STATIC, sz, 0, iY += 26, 170, 22);
-	g_SampleUI.AddSlider(IDC_PUFF_SCALE, 50, iY += 26, 100, 22, 0, 2000, (int)(g_fModelWaviness * 100.0f));
-
-	iY += 24;
-	g_SampleUI.AddCheckBox(IDC_TOGGLESPIN, L"Toggle Spinning", 0, iY += 26, 170, 22, g_bSpinning);
+	m_pGameLogic = VCreateGameAndView();
+	if (m_pGameLogic == nullptr)
+		return false;
 
 	if (hWnd == NULL)
 	{
@@ -210,13 +142,9 @@ bool RenderEngineApp::InitInstance(HINSTANCE hInstance, LPWSTR lpCmdLine, HWND h
 	{
 		m_pRenderer = shared_ptr<IRenderer>(GCC_NEW D3DRenderer11());
 	}
-	m_pRenderer->VSetBackgroundColor(Color(Colors::CornflowerBlue.f));
+	m_pRenderer->VSetBackgroundColor(Color(Colors::MidnightBlue.f));
 	m_pRenderer->VOnRestore();
 
-
-	m_pGameLogic = VCreateGameAndView();
-	if (m_pGameLogic == nullptr)
-		return false;
 
 	m_pResCache->Preload("*.ogg", NULL);
 	m_pResCache->Preload("*.dds", NULL);
@@ -305,114 +233,97 @@ std::wstring RenderEngineApp::GetString(std::wstring sID)
 
 LRESULT CALLBACK RenderEngineApp::MsgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, bool* pbNoFurtherProcessing, void* pUserContext)
 {
-// 	*pbNoFurtherProcessing = D3DRenderer::m_DialogResourceManager.MsgProc(hWnd, uMsg, wParam, lParam);
-// 	if (*pbNoFurtherProcessing)
-// 		return 0;
-// 
-// 	LRESULT result = 0;
-// 
-// 	switch (uMsg)
-// 	{
-// 	case WM_POWERBROADCAST:
-// 	{
-// 		int event = (int)wParam;
-// 		result = g_pApp->OnPowerBroadcast(event);
-// 		break;
-// 	}
-// 
-// 	case WM_DISPLAYCHANGE:
-// 	{
-// 		int colorDepth = (int)wParam;
-// 		int width = (int)(short)LOWORD(lParam);
-// 		int height = (int)(short)HIWORD(lParam);
-// 
-// 		result = g_pApp->OnDisplayChange(colorDepth, width, height);
-// 		break;
-// 	}
-// 
-// 	case WM_SYSCOMMAND:
-// 	{
-// 		result = g_pApp->OnSysCommand(wParam, lParam);
-// 		if (result)
-// 		{
-// 			*pbNoFurtherProcessing = true;
-// 		}
-// 		break;
-// 	}
-// 
-// 	case WM_SYSKEYDOWN:
-// 	{
-// 		if (wParam == VK_RETURN)
-// 		{
-// 			*pbNoFurtherProcessing = true;
-// 			return g_pApp->OnAltEnter();
-// 		}
-// 		return DefWindowProc(hWnd, uMsg, wParam, lParam);
-// 	}
-// 
-// 
-// 	case WM_CLOSE:
-// 	{
-// 		if (g_pApp->m_IsQuitting)
-// 		{
-// 			result = g_pApp->OnClose();
-// 		}
-// 		else
-// 		{
-// 			*pbNoFurtherProcessing = true;
-// 		}
-// 		break;
-// 	}
-// 
-// 
-// 	case WM_KEYDOWN:
-// 	case WM_KEYUP:
-// 	case WM_CHAR:
-// 	case WM_MOUSEMOVE:
-// 	case WM_LBUTTONDOWN:
-// 	case WM_LBUTTONUP:
-// 	case WM_RBUTTONDOWN:
-// 	case WM_RBUTTONUP:
-// 	{
-// 		if (g_pApp->m_pGameLogic)
-// 		{
-// 			BaseGameLogic *pGame = g_pApp->m_pGameLogic;
-// 			AppMsg msg;
-// 			msg.m_hWnd = hWnd;
-// 			msg.m_uMsg = uMsg;
-// 			msg.m_wParam = wParam;
-// 			msg.m_lParam = lParam;
-// 			for (GameViewList::reverse_iterator i = pGame->m_GameViews.rbegin(); i != pGame->m_GameViews.rend(); ++i)
-// 			{
-// 				if ((*i)->VOnMsgProc(msg))
-// 				{
-// 					result = true;
-// 					break;
-// 				}
-// 			}
-// 		}
-// 		break;
-// 	}
-// 	}
-// 
-// 	return result;
-
 	*pbNoFurtherProcessing = D3DRenderer::m_DialogResourceManager.MsgProc(hWnd, uMsg, wParam, lParam);
 	if (*pbNoFurtherProcessing)
 		return 0;
 
-	// Give the dialogs a chance to handle the message first
-	*pbNoFurtherProcessing = g_HUD.MsgProc(hWnd, uMsg, wParam, lParam);
-	if (*pbNoFurtherProcessing)
-		return 0;
-	*pbNoFurtherProcessing = g_SampleUI.MsgProc(hWnd, uMsg, wParam, lParam);
-	if (*pbNoFurtherProcessing)
-		return 0;
+	LRESULT result = 0;
 
-	// Pass all remaining windows messages to camera so it can respond to user input
-// 	g_Camera.HandleMessages(hWnd, uMsg, wParam, lParam);
+	switch (uMsg)
+	{
+	case WM_POWERBROADCAST:
+	{
+		int event = (int)wParam;
+		result = g_pApp->OnPowerBroadcast(event);
+		break;
+	}
 
-	return 0;
+	case WM_DISPLAYCHANGE:
+	{
+		int colorDepth = (int)wParam;
+		int width = (int)(short)LOWORD(lParam);
+		int height = (int)(short)HIWORD(lParam);
+
+		result = g_pApp->OnDisplayChange(colorDepth, width, height);
+		break;
+	}
+
+	case WM_SYSCOMMAND:
+	{
+		result = g_pApp->OnSysCommand(wParam, lParam);
+		if (result)
+		{
+			*pbNoFurtherProcessing = true;
+		}
+		break;
+	}
+
+	case WM_SYSKEYDOWN:
+	{
+		if (wParam == VK_RETURN)
+		{
+			*pbNoFurtherProcessing = true;
+			return g_pApp->OnAltEnter();
+		}
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	}
+
+
+	case WM_CLOSE:
+	{
+		if (g_pApp->m_IsQuitting)
+		{
+			result = g_pApp->OnClose();
+		}
+		else
+		{
+			*pbNoFurtherProcessing = true;
+		}
+		break;
+	}
+
+
+	case WM_KEYDOWN:
+	case WM_KEYUP:
+	case WM_CHAR:
+	case WM_MOUSEMOVE:
+	case WM_LBUTTONDOWN:
+	case WM_LBUTTONUP:
+	case WM_RBUTTONDOWN:
+	case WM_RBUTTONUP:
+	{
+		if (g_pApp->m_pGameLogic)
+		{
+			BaseGameLogic *pGame = g_pApp->m_pGameLogic;
+			AppMsg msg;
+			msg.m_hWnd = hWnd;
+			msg.m_uMsg = uMsg;
+			msg.m_wParam = wParam;
+			msg.m_lParam = lParam;
+			for (GameViewList::reverse_iterator i = pGame->m_GameViews.rbegin(); i != pGame->m_GameViews.rend(); ++i)
+			{
+				if ((*i)->VOnMsgProc(msg))
+				{
+					result = true;
+					break;
+				}
+			}
+		}
+		break;
+	}
+	}
+
+	return result;
 }
 
 LRESULT RenderEngineApp::OnDisplayChange(int colorDepth, int width, int height)
@@ -711,30 +622,10 @@ bool CALLBACK RenderEngineApp::IsD3D11DeviceAcceptable(
 HRESULT CALLBACK RenderEngineApp::OnD3D11CreateDevice(
 	ID3D11Device* pd3dDevice, const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc, void* pUserContext)
 {
-// 	HRESULT hr;
-// 
-// 	ID3D11DeviceContext* pd3dImmediateContext = DXUTGetD3D11DeviceContext();
-// 	V_RETURN(D3DRenderer::m_DialogResourceManager.OnD3D11CreateDevice(pd3dDevice, pd3dImmediateContext));
-// 
-// 	return S_OK;
+	HRESULT hr;
 
-	HRESULT hr = S_OK;
-
-	auto pd3dImmediateContext = DXUTGetD3D11DeviceContext();
+	ID3D11DeviceContext* pd3dImmediateContext = DXUTGetD3D11DeviceContext();
 	V_RETURN(D3DRenderer::m_DialogResourceManager.OnD3D11CreateDevice(pd3dDevice, pd3dImmediateContext));
-	D3DRenderer::m_pTextHelper = new CDXUTTextHelper(pd3dDevice, pd3dImmediateContext, &D3DRenderer::m_DialogResourceManager, 15);
-
-	DWORD dwShaderFlags = D3DCOMPILE_ENABLE_STRICTNESS;
-#ifdef _DEBUG
-	// Set the D3DCOMPILE_DEBUG flag to embed debug information in the shaders.
-	// Setting this flag improves the shader debugging experience, but still allows 
-	// the shaders to be optimized and to run exactly the way they will run in 
-	// the release configuration of this program.
-	dwShaderFlags |= D3DCOMPILE_DEBUG;
-
-	// Disable optimizations to further improve shader debugging
-	dwShaderFlags |= D3DCOMPILE_SKIP_OPTIMIZATION;
-#endif
 
 	return S_OK;
 }
@@ -743,35 +634,18 @@ HRESULT CALLBACK RenderEngineApp::OnD3D11ResizedSwapChain(
 	ID3D11Device* pd3dDevice, IDXGISwapChain* pSwapChain,
 	const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc, void* pUserContext)
 {
-// 	HRESULT hr;
-// 
-// 	V_RETURN(D3DRenderer::m_DialogResourceManager.OnD3D11ResizedSwapChain(pd3dDevice, pBackBufferSurfaceDesc));
-// 
-// 	if (g_pApp->m_pGameLogic)
-// 	{
-// 		BaseGameLogic *pGame = g_pApp->m_pGameLogic;
-// 		for (GameViewList::iterator i = pGame->m_GameViews.begin(); i != pGame->m_GameViews.end(); ++i)
-// 		{
-// 			(*i)->VOnRestore();
-// 		}
-// 	}
-// 
-// 	return S_OK;
-
 	HRESULT hr;
 
 	V_RETURN(D3DRenderer::m_DialogResourceManager.OnD3D11ResizedSwapChain(pd3dDevice, pBackBufferSurfaceDesc));
 
-	// Setup the camera's projection parameters
-	float fAspectRatio = pBackBufferSurfaceDesc->Width / (FLOAT)pBackBufferSurfaceDesc->Height;
-// 	g_Camera.SetProjParams(XM_PI / 4, fAspectRatio, 0.1f, 5000.0f);
-// 	g_Camera.SetWindow(pBackBufferSurfaceDesc->Width, pBackBufferSurfaceDesc->Height);
-// 	g_Camera.SetButtonMasks(MOUSE_LEFT_BUTTON, MOUSE_WHEEL, MOUSE_MIDDLE_BUTTON);
-
-	g_HUD.SetLocation(pBackBufferSurfaceDesc->Width - 170, 0);
-	g_HUD.SetSize(170, 170);
-	g_SampleUI.SetLocation(pBackBufferSurfaceDesc->Width - 170, pBackBufferSurfaceDesc->Height - 300);
-	g_SampleUI.SetSize(170, 300);
+	if (g_pApp->m_pGameLogic)
+	{
+		BaseGameLogic *pGame = g_pApp->m_pGameLogic;
+		for (GameViewList::iterator i = pGame->m_GameViews.begin(); i != pGame->m_GameViews.end(); ++i)
+		{
+			(*i)->VOnRestore();
+		}
+	}
 
 	return S_OK;
 }
@@ -780,39 +654,15 @@ void CALLBACK RenderEngineApp::OnD3D11FrameRender(
 	ID3D11Device* pd3dDevice, ID3D11DeviceContext* pd3dImmediateContext,
 	double fTime, float fElapsedTime, void* pUserContext)
 {
-// 	BaseGameLogic *pGame = g_pApp->m_pGameLogic;
-// 
-// 	for (GameViewList::iterator i = pGame->m_GameViews.begin(),
-// 		end = pGame->m_GameViews.end(); i != end; ++i)
-// 	{
-// 		(*i)->VOnRender(fTime, fElapsedTime);
-// 	}
-// 
-// 	g_pApp->m_pGameLogic->VRenderDiagnostics();
+	BaseGameLogic *pGame = g_pApp->m_pGameLogic;
 
-	//
-	// Clear the back buffer
-	//
-	auto pRTV = DXUTGetD3D11RenderTargetView();
-	pd3dImmediateContext->ClearRenderTargetView(pRTV, Colors::MidnightBlue);
+	for (GameViewList::iterator i = pGame->m_GameViews.begin(),
+		end = pGame->m_GameViews.end(); i != end; ++i)
+	{
+		(*i)->VOnRender(fTime, fElapsedTime);
+	}
 
-	//
-	// Clear the depth stencil
-	//
-	auto pDSV = DXUTGetD3D11DepthStencilView();
-	pd3dImmediateContext->ClearDepthStencilView(pDSV, D3D11_CLEAR_DEPTH, 1.0, 0);
-
-	//
-	// Render the UI
-	//
-	g_HUD.OnRender(fElapsedTime);
-	g_SampleUI.OnRender(fElapsedTime);
-	D3DRenderer::m_pTextHelper->Begin();
-	D3DRenderer::m_pTextHelper->SetInsertionPos(2, 0);
-	D3DRenderer::m_pTextHelper->SetForegroundColor(Colors::Yellow);
-	D3DRenderer::m_pTextHelper->DrawTextLine(DXUTGetFrameStats(DXUTIsVsyncEnabled()));
-	D3DRenderer::m_pTextHelper->DrawTextLine(DXUTGetDeviceStats());
-	D3DRenderer::m_pTextHelper->End();
+	g_pApp->m_pGameLogic->VRenderDiagnostics();
 }
 
 void CALLBACK RenderEngineApp::OnD3D11ReleasingSwapChain(void* pUserContext)
@@ -827,7 +677,6 @@ void CALLBACK RenderEngineApp::OnD3D11DestroyDevice(void* pUserContext)
 	D3DRenderer::m_DialogResourceManager.OnD3D11DestroyDevice();
 	g_pApp->m_pRenderer = shared_ptr<IRenderer>(NULL);
 
-// 	D3DRenderer::m_DialogResourceManager.OnD3D11DestroyDevice();
 	DXUTGetGlobalResourceCache().OnDestroyDevice();
 }
 
@@ -848,26 +697,25 @@ bool CALLBACK RenderEngineApp::ModifyDeviceSettings(DXUTDeviceSettings* pDeviceS
 
 void CALLBACK RenderEngineApp::OnUpdateGame(double fTime, float fElapsedTime, void* pUserContext)
 {
-// 	if (g_pApp->HasModalDialog())
-// 	{
-// 		return;
-// 	}
-// 
-// 	if (g_pApp->m_IsQuitting)
-// 	{
-// 		PostMessage(g_pApp->GetHwnd(), WM_CLOSE, 0, 0);
-// 	}
-// 
-// 	if (g_pApp->m_pGameLogic)
-// 	{
-// 		IEventManager::Get()->VUpdate(20);
-// 
-// // 		if (g_pApp->m_pBaseSocketManager)
-// // 			g_pApp->m_pBaseSocketManager->DoSelect(0);
-// 
-// 		g_pApp->m_pGameLogic->VOnUpdate(float(fTime), fElapsedTime);
-// 	}
+	if (g_pApp->HasModalDialog())
+	{
+		return;
+	}
 
+	if (g_pApp->m_IsQuitting)
+	{
+		PostMessage(g_pApp->GetHwnd(), WM_CLOSE, 0, 0);
+	}
+
+	if (g_pApp->m_pGameLogic)
+	{
+		IEventManager::Get()->VUpdate(20);
+
+// 		if (g_pApp->m_pBaseSocketManager)
+// 			g_pApp->m_pBaseSocketManager->DoSelect(0);
+
+		g_pApp->m_pGameLogic->VOnUpdate(float(fTime), fElapsedTime);
+	}
 }
 
 bool RenderEngineApp::AttachAsClient()

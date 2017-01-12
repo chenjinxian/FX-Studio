f7d17b84f14ff28e0593011651ad4c74cd5f68d6
chenjinxian <alanchen1203@gmail.com>
2017-01-12 11:14:25 +0800

    Revert "Revert "to do""
    This reverts commit 33d91e32a94754bb9b3d4e1dd6370f4ebe15ed12.

--- Source/TinyEngine/Graphics3D/DebugGizmosNode.h
+++ Source/TinyEngine/Graphics3D/DebugGizmosNode.h
@@ -49,6 +49,7 @@ private:
 	bool IsAxisPicked(const Ray& ray);
 	bool IsTorusPicked(const Ray& ray);
 	Vector3 IntersectRayPlane(Scene* pScene, const Matrix& world);
+	Vector3 IntersectRaySphere(Scene* pScene, const Matrix& world);
 
 	Effect* m_pEffect;
 	Pass* m_pCurrentPass;

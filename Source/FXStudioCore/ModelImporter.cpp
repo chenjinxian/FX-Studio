a96cb306ea76053964094f17fa3ae7084921e0f2
chenjinxian <alanchen1203@gmail.com>
2017-02-03 23:53:37 +0800

    to do

--- Source/FXStudioCore/ModelImporter.cpp
+++ Source/FXStudioCore/ModelImporter.cpp
@@ -55,5 +55,43 @@ void ModelImporter::LoadModer(const std::string& filePath, ProgressCallback call
 		return;
 	}
 
+	tinyxml2::XMLDocument xmlDoc;
+
+	tinyxml2::XMLElement* pRoot = xmlDoc.NewElement("Model");
+	xmlDoc.InsertEndChild(pRoot);
+	WriteNode(scene->mRootNode, &xmlDoc, pRoot, 0);
+
+	tinyxml2::XMLPrinter printer;
+	xmlDoc.Accept(&printer);
+
 	m_AssimpImporter->FreeScene();
+}
+
+void ModelImporter::WriteNode(const aiNode* pSceneNode, tinyxml2::XMLDocument* pXmlDoc, tinyxml2::XMLElement* pXmlNode, uint32_t depth)
+{
+	tinyxml2::XMLElement* pRoot = pXmlDoc->NewElement("Node");
+	pXmlNode->InsertEndChild(pRoot);
+	tinyxml2::XMLElement* pMatrix = pXmlDoc->NewElement("Matrix");
+	pRoot->InsertEndChild(pMatrix);
+
+	const aiMatrix4x4& mat = pSceneNode->mTransformation;
+	char buffer[4096];
+	sprintf_s(buffer, "%f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f",
+		mat.a1, mat.a2, mat.a3, mat.a4, mat.b1, mat.b2, mat.b3, mat.b4, mat.c1, mat.c2, mat.c3, mat.c4, mat.d1, mat.d2, mat.d3, mat.d4);
+
+	if (pSceneNode->mNumMeshes)
+	{
+
+	}
+	
+	if (pSceneNode->mNumChildren)
+	{
+		tinyxml2::XMLElement* pList = pXmlDoc->NewElement("NodeList");
+		pRoot->InsertEndChild(pList);
+
+		for (uint32_t i = 0; i < pSceneNode->mNumChildren; i++)
+		{
+			WriteNode(pSceneNode->mChildren[i], pXmlDoc, pList, depth + 2);
+		}
+	}
 }
--- Source/FXStudioCore/ModelImporter.h
+++ Source/FXStudioCore/ModelImporter.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "assimp/Importer.hpp"
+#include "assimp/scene.h"
 #include "assimp/ProgressHandler.hpp"
 #include "boost/noncopyable.hpp"
+#include "tinyxml2.h"
 #include <memory>
 
 typedef bool(*ProgressCallback)(float, const char*);
@@ -20,6 +22,8 @@ protected:
 	virtual bool Update(float percentage = -1.f) override;
 
 private:
+	void WriteNode(const aiNode* sceneNode, tinyxml2::XMLDocument* xmlDoc, tinyxml2::XMLElement* xmlNode, uint32_t depth);
+
 	std::unique_ptr<Assimp::Importer> m_AssimpImporter;
 	ProgressCallback m_Callback;
 };

6aaa8a140e60e36521a459fe1c22572c3b8c8949
chenjinxian <alanchen1203@gmail.com>
2017-02-06 23:53:19 +0800

    Revert "Revert "to do""
    This reverts commit bd9fffe8a66c0f49254252e162ce8317281e1df2.

--- Source/FXStudio/ProjectView.cs
+++ Source/FXStudio/ProjectView.cs
@@ -5,7 +5,6 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Forms;
 using System.Xml;
--- Source/FXStudio/ToolStripExRenderer.cs
+++ Source/FXStudio/ToolStripExRenderer.cs
@@ -4,7 +4,6 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FXStudio
